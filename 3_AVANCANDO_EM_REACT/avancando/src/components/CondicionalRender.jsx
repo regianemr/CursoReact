f513e403b6b65264e04eea4e264d0d497ec0bc9b
Regiane <regianemelo1995@gmail.com>
2024-06-28 15:25:10 -0300

    Else

--- 3_AVANCANDO_EM_REACT/avancando/src/components/CondicionalRender.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/CondicionalRender.jsx
@@ -3,11 +3,24 @@ import React from 'react'
 const CondicionalRender = () => {
     const x = 5
 
+    const name = "Maria"
+
   return (
     <div>
         {/* 7- Render Condicional */}
         <h3>Isso será exibido?</h3>
         {x > 2 && <p>Se x for true, sim!</p>}
+        {/* 8 - Else */}
+        <h3>Render ternário</h3>
+        {name === "Maria" ?(
+            <div>
+                <p>Olá, Maria</p>
+            </div>
+        ) : (
+            <div>
+                <p>Nome não encontrado</p>
+            </div>
+        )}
     </div>
   )
 }
