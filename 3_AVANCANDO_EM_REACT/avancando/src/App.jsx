fb54c2d36816db1c454f876f734182a5ec845cac
Regiane <regianemelo1995@gmail.com>
2024-07-01 12:47:23 -0300

    Renderizacao e reutilizacao de componentes

--- 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -12,6 +12,20 @@ import ListRender from './components/ListRender'
 // 7 - Render condicional
 import CondicionalRender from './components/CondicionalRender'
 
+// 8 - props
+import ShowUserName from './components/ShowUserName'
+
+// 9 - Desestruturando props
+import CarDetails from './components/CarDetails'
+
+// 11 - Renderização de listas com componente
+const cars = [
+  {id: 1, brand: "Ferrari", color: "Amarelo", km: 0 },
+  {id: 2, brand: "KIA", color: "Preto", km: 122222 },
+  {id: 3, brand: "Renault", color: "Cinza", km: 525555 },
+]
+
+
 function App() {
 
   return (
@@ -27,6 +41,17 @@ function App() {
         <ListRender/>
         {/* 7 - Render Condicional */}
         <CondicionalRender />
+        {/* 8 - Props */}
+        <ShowUserName name="Regiane"/>
+        {/* 9 - Desestruturando props */}
+        <CarDetails brand="VW" km={2546}color="Branco"/>
+        {/* 10 - Reaproveitamento de componentes */}
+        <CarDetails brand="Fiat" km={8685} color="Prata"/>
+        <CarDetails km={9415} color="Vermelho" brand="Audi"/>
+        {/* 11 - Renderização de lista com componentes (loop) */}
+        {cars.map((car) => (
+          <CarDetails key={car.id} brand={car.brand} color={car.color} km={car.km}/>
+        ))}
       </div>
       
   )
