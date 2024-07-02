599a59d45c7f25a3fb226f7a72890e90e9879dee
Regiane <regianemelo1995@gmail.com>
2024-07-02 15:24:29 -0300

    criando formularios

---
+++ 5_FORM_REACT/form/src/components/MyForm.jsx
@@ -0,0 +1,23 @@
+import "./MyForm.css"
+
+const MyForm = () => {
+  return (
+    <div>
+        {/* 1 - Criação de form */}
+        <form>
+            <div>
+                <label htmlFor="name">Nome:</label>
+                <input type="text" name="name" placeholder="Digite o seu nome" />
+            </div>
+            {/* 2 - label envolvendo input */}
+            <label>
+                <span>E-mail:</span>
+                <input type="text" name= "email" placeholder="Digite seu e-mail" />
+            </label>
+            <input type="submit" value="Enviar" />
+        </form>
+    </div>
+  )
+}
+
+export default MyForm
