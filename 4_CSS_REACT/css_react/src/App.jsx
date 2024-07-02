051439f8846492ac5cd46d4ebd5687a762204d7a
Regiane <regianemelo1995@gmail.com>
2024-07-02 10:25:28 -0300

    css do react

---
+++ 4_CSS_REACT/css_react/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+// 2 - css de componente
+import MyComponent from './components/MyComponent'
+
+
+function App() {
+  
+
+  return (
+      <div className='App'>
+        {/* 1- css global */}
+        <h1>CSS no React</h1>
+        {/* 2- css de componente */}
+        <MyComponent />
+        <p>Pegou o CSS do componente</p>
+        
+      </div>
+      
+  )
+}
+
+export default App
