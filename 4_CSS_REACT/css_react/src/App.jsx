f585d1cdfc70c38d290e13f492b0f43e01b8e600
Regiane <regianemelo1995@gmail.com>
2024-07-02 11:03:08 -0300

    css modules

--- 4_CSS_REACT/css_react/src/App.jsx
+++ 4_CSS_REACT/css_react/src/App.jsx
@@ -2,17 +2,21 @@ import './App.css'
 
 // 2 - css de componente
 import MyComponent from './components/MyComponent'
+import Title from './components/Title'
 
 
 function App() {
   // 4 - css inline dinamico
   const n = 15
+
+  // 5 - classes dinamicas
+  const redTitle = true
   
 
   return (
       <div className='App'>
         {/* 1- css global */}
-        <h1>CSS no React</h1>
+        <h1 className='title'>CSS no React</h1>
         {/* 2- css de componente */}
         <MyComponent />
         <p>Pegou o CSS do componente</p>
@@ -28,6 +32,14 @@ function App() {
         <h2 style={n > 20 ? {color: "pink"}: {color: "magenta" }}>
           CSS dinâmico 2
         </h2>
+        {/* 5 - classes dinâmicas */}
+        <h2 className={redTitle ? "red-title" : "title"}
+        >
+          Este título vai ter uma classe
+        </h2>
+        {/* 6 - css modulos */}
+        <Title />
+
         
       </div>
       
