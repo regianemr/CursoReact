85670ca4b524a8add6bc330d05f69858ab833db2
Regiane <regianemelo1995@gmail.com>
2024-07-02 17:22:13 -0300

    salvando dados

--- 5_FORM_REACT/form/src/components/MyForm.jsx
+++ 5_FORM_REACT/form/src/components/MyForm.jsx
@@ -1,18 +1,34 @@
 import "./MyForm.css"
 
+import { useState } from "react"
+
 const MyForm = () => {
+    // 3 - gerenciamento de dados
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+
+    console.log(name)
+
   return (
     <div>
         {/* 1 - Criação de form */}
         <form>
             <div>
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" placeholder="Digite o seu nome" />
+                <input type="text" name="name" 
+                placeholder="Digite o seu nome" 
+                onChange={handleName} />
             </div>
             {/* 2 - label envolvendo input */}
             <label>
                 <span>E-mail:</span>
-                <input type="text" name= "email" placeholder="Digite seu e-mail" />
+                <input type="text" 
+                name= "email" 
+                placeholder="Digite seu e-mail" />
             </label>
             <input type="submit" value="Enviar" />
         </form>
