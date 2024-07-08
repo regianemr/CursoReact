48343df49886361b7a27358ed1598ecde856296c
Regiane <regianemelo1995@gmail.com>
2024-07-08 11:43:44 -0300

    http react

---
+++ 6_HTTP_REACT/http_react/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react"
+
+const url = "http://localhost:3000/products"
+
+import './App.css'
+
+function App() {
+  // 1 resgatando dados
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    
+    async function getData() {
+      const res = await fetch(url)
+
+      const data = await res.json()
+
+      setProducts(data)
+    }
+
+    getData()
+
+  }, [])
+
+  return (
+      <div className='App'>
+       <h1>HTTP em React</h1>
+       {/* 1 - Resgate de dados */}
+       <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R${product.price}
+          </li>
+        ))}
+       </ul>
+      </div>
+      
+  )
+}
+
+export default App
