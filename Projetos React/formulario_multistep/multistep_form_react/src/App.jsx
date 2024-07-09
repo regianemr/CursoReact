d55b2bcd994965ab12c9a02e69d45f35562df766
Regiane <regianemelo1995@gmail.com>
2024-07-09 16:09:29 -0300

    mudando de etapas

--- Projetos React/formulario_multistep/multistep_form_react/src/App.jsx
+++ Projetos React/formulario_multistep/multistep_form_react/src/App.jsx
@@ -1,5 +1,7 @@
 // Componentes
 import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+import { FiSend } from 'react-icons/fi'
+
 import UserForm from "./components/UserForm"
 import ReviewForm from "./components/ReviewForm"
 import Thanks  from "./components/Thanks"
@@ -17,7 +19,7 @@ function App() {
     <Thanks />
   ]
 
-  const { currentStep, currentComponent } = useForm(formComponents)
+  const { currentStep, currentComponent, changesStep, isLastStep, isFirstStep } = useForm(formComponents)
 
   return (
    <div className="App">
@@ -30,17 +32,26 @@ function App() {
       </div>
       <div className="form-container">
         <p>etapas</p>
-        <form>
+        <form onSubmit={(e) => changesStep(currentStep +1, e)}>
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
-            <button type='button'>
+            {!isFirstStep && (
+              <button type='button' onClick={() => changesStep(currentStep -1)}>
               <GrFormPrevious />
               <span>Voltar</span>
             </button>
-            <button type='submit'>
+            )}
+            {!isLastStep ? (
+              <button type='submit'>
               <span>Avançar</span>
               <GrFormNext />
             </button>
+            ) : (
+              <button type='button'>
+              <span>Enviar</span>
+              <FiSend />
+            </button>
+            )}
           </div>
         </form>
       </div>
