35ae96fc258941220b4f49419fda8b282515e496
Regiane <regianemelo1995@gmail.com>
2024-07-16 12:33:40 -0300

    subindo a pasta

--- Projetos React/formulario_multistep/src/App.jsx
+++ Projetos React/formulario_multistep/src/App.jsx
@@ -5,18 +5,34 @@ import { FiSend } from 'react-icons/fi'
 import UserForm from "./components/UserForm"
 import ReviewForm from "./components/ReviewForm"
 import Thanks  from "./components/Thanks"
+import Steps from "./components/Steps"
 
 // Hooks
 import { useForm } from './hooks/useForm'
+import { useState } from 'react'
 
 import './App.css'
 
+const formTemplate = {
+  name: "",
+  email: "",
+  review: "",
+  comment: "",
+}
+
 function App() {
+  const [data, setData] = useState(formTemplate)
+
+  const updateFieldHandler = (key, value) => {
+    setData((prev) => {
+      return {...prev, [key]: value}
+    })
+  }
 
   const formComponents = [
-    <UserForm />,
-    <ReviewForm />,
-    <Thanks />
+    <UserForm data={data} updateFieldHandler={updateFieldHandler} />,
+    <ReviewForm data={data} updateFieldHandler={updateFieldHandler} />,
+    <Thanks data={data} />
   ]
 
   const { currentStep, currentComponent, changesStep, isLastStep, isFirstStep } = useForm(formComponents)
@@ -31,7 +47,7 @@ function App() {
         </p>
       </div>
       <div className="form-container">
-        <p>etapas</p>
+        <Steps currentStep={currentStep} /> 
         <form onSubmit={(e) => changesStep(currentStep +1, e)}>
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
