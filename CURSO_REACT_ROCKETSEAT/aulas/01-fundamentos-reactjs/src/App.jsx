7a5c609a5efe809b4a5c9ad89a1bb0be69065958
Regiane <regianemelo1995@gmail.com>
2024-08-05 15:38:03 -0300

    iniciando css da pagina

--- CURSO_REACT_ROCKETSEAT/aulas/01-fundamentos-reactjs/src/App.jsx
+++ CURSO_REACT_ROCKETSEAT/aulas/01-fundamentos-reactjs/src/App.jsx
@@ -1,8 +1,27 @@
-function App() {
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+import './global.css'
+
+ export function App() {
 
   return (
-    <h1>Hello Word</h1>
+    <div>
+      <Header />
+
+
+      <Post 
+      author="Regiane" 
+      content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero at sed repellendus necessitatibus sit beatae delectus nisi officiis reprehenderit cumque dolore quae quis tenetur, iste nulla. Officia quaerat distinctio sed."
+      />
+
+      <Post
+      author="Gleison"
+      content="Outro post"
+      />
+
+    </div>
+    
   )
 }
 
-export default App
