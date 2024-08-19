28bf8b7c5e8aa046ba0154b15288a62a5825a877
Regiane <regianemelo1995@gmail.com>
2024-08-19 11:43:43 -0300

    fundamentos do react em typescript

--- CURSO_REACT_ROCKETSEAT/aulas/01-fundamentos-react-js-ts/src/components/Comment.tsx
+++ CURSO_REACT_ROCKETSEAT/aulas/01-fundamentos-react-js-ts/src/components/Comment.tsx
@@ -3,9 +3,12 @@ import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
 import { useState } from 'react'
 
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void
+}
 
-
-export function Comment({ content, onDeleteComment }) {
+export function Comment({ content, onDeleteComment }: CommentProps) {
     const [likeCount, setLikeCount] = useState(0)
 
     function handleDeleteComment() {
@@ -21,7 +24,11 @@ export function Comment({ content, onDeleteComment }) {
 
     return (
         <div className={styles.comment}>
-            <Avatar hasBorder={false} src="https://github.com/regianemr.png" alt="" />
+            <Avatar 
+                hasBorder={false} 
+                src="https://github.com/regianemr.png" 
+                alt="" 
+            />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
---
+++ CURSO_REACT_ROCKETSEAT/aulas/01-fundamentos-react-js-ts/src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import { PencilSimpleLine } from 'phosphor-react'
+import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
+
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} 
+            src="https://plus.unsplash.com/premium_photo-1661882403999-46081e67c401?w=500&auto=format&fit=crop&q=50&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZ3JhbWElQzMlQTclQzMlQTNvfGVufDB8fDB8fHww"  
+            />
+        
+            <div className={styles.profile}>
+                <Avatar src="https://github.com/regianemr.png" />
+
+                <strong>Regiane Melo</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+               <a href="#">
+                <PencilSimpleLine size={20} />
+                Editar seu perfil
+                </a> 
+            </footer>
+        </aside>
+    )
+}
