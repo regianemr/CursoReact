39f9c7a0fdaffefaee96133ab9a17c79c2e59403
Regiane <regianemelo1995@gmail.com>
2024-08-20 15:22:00 -0300

    configurando temas

--- CURSO_REACT_ROCKETSEAT/aulas/02-ignite-timer/src/components/Button.styles.ts
+++ CURSO_REACT_ROCKETSEAT/aulas/02-ignite-timer/src/components/Button.styles.ts
@@ -17,10 +17,17 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
 
     width: 100px;
     height: 40px;
+    border-radius: 4px;
+    border: 0;
+    margin: 8px;
 
-    ${props => {
-        return css `
+
+    background-color: ${props => props.theme.primary};
+    color: ${props => props.theme.white}
+
+    /* ${props => {
+        return css`
             background-color: ${buttonVariants[props.variant]}
         `
-    }}
+    }} */
 `
