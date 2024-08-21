ca6ce02897f7e48c762515270e54e91d471f1c5f
Regiane <regianemelo1995@gmail.com>
2024-08-21 12:29:01 -0300

    eslint

--- CURSO_REACT_ROCKETSEAT/aulas/02-ignite-timer/src/@types/styles.d.ts
+++ CURSO_REACT_ROCKETSEAT/aulas/02-ignite-timer/src/@types/styles.d.ts
@@ -5,5 +5,6 @@ import { defaultTheme } from '../styles/themes/default'
 type ThemeType = typeof defaultTheme;
 
 declare module 'styled-components' {
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     export interface defaultTheme extends ThemeType {}
 }
