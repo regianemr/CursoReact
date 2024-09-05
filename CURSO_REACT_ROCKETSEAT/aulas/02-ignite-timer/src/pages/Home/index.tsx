6be09e225dd349a23e82556b3e2cab76b4a162e7
Regiane <regianemelo1995@gmail.com>
2024-09-05 10:31:01 -0300

    interrompendo o ciclo

--- CURSO_REACT_ROCKETSEAT/aulas/02-ignite-timer/src/pages/Home/index.tsx
+++ CURSO_REACT_ROCKETSEAT/aulas/02-ignite-timer/src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
  
-import { Play } from "phosphor-react";
+import { HandPalm, Play } from "phosphor-react";
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
@@ -13,6 +13,7 @@ import {
     MinutesAmountImput, 
     Separator, 
     StartCountdownButton, 
+    StopCountdownButton, 
     TaskInput
 } from "./styles";
 
@@ -31,6 +32,7 @@ interface Cycle {
     task: string;
     minutesAmount: number
     startDate: Date
+    interruptedDate?: Date
 }
 
 export function Home() {
@@ -49,12 +51,18 @@ export function Home() {
     const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
     useEffect(() => {
+        let interval: number
+
         if (activeCycle) {
-            setInterval(() => {
+            interval = setInterval(() => {
                 setAmountSecondPassed(
                     differenceInSeconds(new Date(), activeCycle.startDate),
                 )
             }, 1000)
+        }
+
+        return () => {
+            clearInterval(interval)
         }
     }, [activeCycle])
 
@@ -70,11 +78,24 @@ export function Home() {
 
         setCycles((state) => [...state, newCycle])
         setActiveCycleId(id)
+        setAmountSecondPassed(0)
 
         reset()
     }
 
-    
+    // função para pausar o ciclo
+    function handleInterruptCycle() {
+        setCycles(
+            cycles.map((cycle) => {
+                if (cycle.id === activeCycleId) {
+                    return {...cycle, interruptedDate: new Date() }
+                } else {
+                    return cycle
+                }
+            }),
+        )
+        setActiveCycleId(null)
+    }
     
     // convertendo minutos em segundos
     const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
@@ -87,10 +108,19 @@ export function Home() {
     const minutes = String(minutesAmount).padStart(2, '0')
     const seconds = String(secondsAmount).padStart(2, '0')
 
+    // colocando o timer no título da aba
+    useEffect(() => {
+        if (activeCycle) {
+            document.title = `${minutes}:${seconds}`
+        }
+        
+    }, [minutes, seconds, activeCycle])
+
 
     const task = watch('task')
     const isSubmitDisabled = !task;
 
+    console.log(cycles)
 
     return (
         <HomeContainer>
@@ -101,6 +131,7 @@ export function Home() {
                     id="task" 
                     list="task-suggestions"
                     placeholder="Dê um nome para o seu projeto"
+                    disabled={!!activeCycle}
                     {...register('task')}
                 />
 
@@ -119,6 +150,7 @@ export function Home() {
                     step={5}
                     min={5}
                     max={60}
+                    disabled={!!activeCycle}
                     {...register('minutesAmount', { valueAsNumber: true })}
                 />
 
@@ -133,10 +165,18 @@ export function Home() {
                     <span>{seconds[1]}</span>
                 </CountdownContainer>
 
-                <StartCountdownButton disabled={isSubmitDisabled} type="submit">
-                    <Play size={24} />
+                {/* botão do novo cilo */}
+                { activeCycle ? (
+                    <StopCountdownButton onClick={handleInterruptCycle} type="button">
+                        <HandPalm size={24} />
+                        Interromper
+                    </StopCountdownButton>
+                ) : (
+                    <StartCountdownButton disabled={isSubmitDisabled} type="submit">
+                        <Play size={24} />
                     Começar
-                </StartCountdownButton>
+                    </StartCountdownButton>
+                )}
             </form>
         </HomeContainer>
     )
